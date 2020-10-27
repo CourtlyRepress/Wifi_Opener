6c1b2d245bbcb56e1fa81562680dd90bb6427825
rhythm98 <rhyagr@gmail.com>
2020-10-27 13:09:22 +0530

    Error Resolved.

--- app/src/main/java/com/example/wifiopener/MainActivity.java
+++ app/src/main/java/com/example/wifiopener/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.wifiopener;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -15,15 +20,16 @@ import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private SwitchCompat wifiSwitch;
     private WifiManager wifiManager;
-    ListView wifiDevices;
-    ArrayList<String>devices;
-    ArrayList<ScanResult>wifiList;
+    ListView wifiDevices;//wifiList
+//    ArrayList<String>devices;
+    List<ScanResult> wifiList;//mywifiList
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +37,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         wifiDevices =findViewById(R.id.lv_availableDevices);
-        WifiAdapter wifiAdapter =new WifiAdapter();
-        wifiDevices.setAdapter(wifiAdapter);
+//        wifiAdapter =new WifiAdapter();
+//        wifiDevices.setAdapter(wifiAdapter);
 
         wifiSwitch = findViewById(R.id.wifi_switch);
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiReceiver wifiReceiver = new WifiReceiver();
+        registerReceiver(wifiReceiver,new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
+        }else{
+            scanWifiList();
+        }
         wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -49,8 +61,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
+
+    private void scanWifiList() {
+        wifiManager.startScan();
+        wifiList= wifiManager.getScanResults();
+        WifiAdapter wifiAdapter = new WifiAdapter();
+        wifiDevices.setAdapter(wifiAdapter);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -79,8 +98,17 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    public class WifiAdapter extends BaseAdapter {
-
+    public class WifiAdapter/*ListAdapter*/ extends BaseAdapter {
+        /*
+        Context context;
+        LayoutInflater inflater;
+        List<ScanResult>wifiList;
+        public WifiAdapter(Context context,List<ScanResult>wifiList){
+            this.context=context;
+            this.wifiList=wifiList;
+            inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        */
         @Override
         public int getCount() {
             return wifiList.size();
@@ -88,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public String getItem(int position) {
-            return devices.get(position);
+            return null;
         }
 
         @Override
@@ -99,22 +127,34 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             DeviceViewHolder holder;
+//            View view=convertView;
             if(convertView == null){
+//                view=inflater.inflate(R.layout.list_item,null);
                 convertView = getLayoutInflater().inflate(R.layout.list_item,parent,false);
+//                holder = new Holder();
                 holder=new DeviceViewHolder(convertView);
+//                holder.tvDeviceName=(TextView)convertView.findViewById(R.id.tv_wifiDevice);
                 convertView.setTag(holder);
             }else{
                 holder=(DeviceViewHolder)convertView.getTag();
             }
-            String device=getItem(position);
-            holder.tv_wifiDevice.setText(device);
+//            String device=getItem(position);
+            holder.tvDeviceName.setText(wifiDevices.get(position).SSID);
             return convertView;
         }
+
+        //Holder class defined
         class DeviceViewHolder {
-            TextView tvDeviceName;
+            TextView tvDeviceName;//tvDetails
             DeviceViewHolder (View convertView){
                 tvDeviceName=convertView.findViewById(R.id.tv_wifiDevice);
             }
+        }
+    }
+    static class WifiReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
         }
     }
 }
